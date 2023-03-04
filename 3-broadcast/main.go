cb30884b2acbaa0f9e67c7f66cd8dd41accf9585
Miccah Castorina <m.castorina93@gmail.com>
2023-03-03 22:45:02 -0600

    Implement batching for broadcast challenge
    This significantly reduces the number of messages per operation, but
    latency remains high. I think our linear topology is the issue because
    adding nodes increases the latency. Logically this makes sense if you
    consider the number of hops it takes for a message from the first node
    to reach the last node.

--- 3-broadcast/main.go
+++ 3-broadcast/main.go
@@ -14,10 +14,22 @@ import (
 
 func main() {
 	n := maelstrom.NewNode()
-	var topology []string
-	messages := NewSet[int]()
+
+	// Our neighbor we send messages to.
+	var friend string
+
+	// Total list of seen messages.
+	seenMessages := NewSet[int]()
+
+	// Cache to batch messages.
+	cache := NewCache[int]()
+	defer cache.Wait()
+	// Context for stopping the cache.
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		// Broadcast is a message received from a client.
 		type broadcastMsg struct {
 			Kind    string `json:"type"`
 			Message int    `json:"message"`
@@ -27,27 +39,31 @@ func main() {
 			return err
 		}
 
-		// If we haven't seen this message before, share what we
-		// learned to our friends.
-		if messages.Add(body.Message) {
-			for _, friend := range topology {
-				if friend == msg.Src {
-					continue
-				}
-				friend := friend
-				go Retry(context.Background(), func(ctx context.Context) error {
-					ctx, cancel := context.WithTimeout(ctx, 250*time.Millisecond)
-					defer cancel()
-					_, err := n.SyncRPC(ctx, friend, body)
-					return err
-				})
+		// Record the message and add it to our cache of batch messages.
+		seenMessages.Add(body.Message)
+		cache.Add(body.Message)
+		return n.Reply(Ack(msg))
+	})
+
+	n.Handle("batch", func(msg maelstrom.Message) error {
+		// Batch is an internal batched broadcast sent between nodes.
+		var body BatchMessage
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		for _, message := range body.Messages {
+			// If it's a new message, add it to our batch to send to our friend.
+			if seenMessages.Add(message) {
+				cache.Add(message)
 			}
 		}
+
 		return n.Reply(Ack(msg))
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
-		return n.Reply(Ack(msg, "messages", messages.Elements()))
+		return n.Reply(Ack(msg, "messages", seenMessages.Elements()))
 	})
 
 	n.Handle("topology", func(msg maelstrom.Message) error {
@@ -55,13 +71,32 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		topology = []string{fmt.Sprintf("n%d", (num+1)%len(n.NodeIDs()))}
+		// Set friend globally to be used by other handles.
+		friend = fmt.Sprintf("n%d", (num+1)%len(n.NodeIDs()))
+		cache.SetFlush(ctx, 10*time.Millisecond, func(elems []int) {
+			// When the cache is flushed, send all elements to our friend.
+			go Retry(ctx, func(ctx context.Context) error {
+				body := BatchMessage{
+					Kind:     "batch",
+					Messages: elems,
+				}
+				ctx, cancel := context.WithTimeout(ctx, 250*time.Millisecond)
+				defer cancel()
+				_, err := n.SyncRPC(ctx, friend, body)
+				return err
+			})
+		})
 		return n.Reply(Ack(msg))
 	})
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+type BatchMessage struct {
+	Kind     string `json:"type"`
+	Messages []int  `json:"messages"`
 }
 
 func NoopHandler(maelstrom.Message) error { return nil }
@@ -124,4 +159,66 @@ func (s *Set[T]) Elements() []T {
 		elems = append(elems, elem)
 	}
 	return elems
+}
+
+func (s *Set[T]) Clear() []T {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	elems := make([]T, 0, len(s.elements))
+	for elem := range s.elements {
+		elems = append(elems, elem)
+	}
+	s.elements = make(map[T]struct{})
+	return elems
+}
+
+type Cache[T comparable] struct {
+	elements Set[T]
+	flush    func(elems []T)
+	wg       sync.WaitGroup
+}
+
+func NewCache[T comparable]() Cache[T] {
+	return Cache[T]{elements: NewSet[T]()}
+}
+
+func (c *Cache[T]) SetFlush(ctx context.Context, timeout time.Duration, flush func([]T)) {
+	c.flush = flush
+	c.wg.Add(1)
+	go func() {
+		defer c.wg.Done()
+		c.worker(ctx, timeout)
+	}()
+}
+
+func (c *Cache[T]) Add(t T) {
+	c.elements.Add(t)
+}
+
+func (c *Cache[T]) Flush() {
+	if c.flush == nil {
+		return
+	}
+	// Clear the set and call flush if there were any elements.
+	if elems := c.elements.Clear(); len(elems) > 0 {
+		c.flush(elems)
+	}
+}
+
+func (c *Cache[T]) Wait() {
+	c.wg.Wait()
+}
+
+func (c *Cache[T]) worker(ctx context.Context, timeout time.Duration) {
+	ticker := time.NewTicker(timeout)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			c.Flush()
+			return
+		case <-ticker.C:
+			c.Flush()
+		}
+	}
 }
