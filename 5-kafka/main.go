1bbb4f13ce10c0791422d723ef1a69e6fb60929d
Miccah Castorina <m.castorina93@gmail.com>
2023-03-11 21:45:30 -0600

    Working multi-node implementation for challenge 5b

--- 5-kafka/main.go
+++ 5-kafka/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"log"
-	"sync"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 func main() {
 	n := maelstrom.NewNode()
-	kafka := NewKafka()
+	kafka := NewKafka(maelstrom.NewLinKV(n))
 
 	n.Handle("send", func(msg maelstrom.Message) error {
 		body, err := Parse[struct {
@@ -58,56 +60,110 @@ func main() {
 	}
 }
 
+// "key": []int
+// "commit-offsets": {"key": int}
 type Kafka struct {
-	mu               sync.Mutex
-	messages         map[string][]int
-	committedOffsets map[string]int
+	kv *maelstrom.KV
 }
 
-func NewKafka() Kafka {
-	return Kafka{
-		messages:         map[string][]int{},
-		committedOffsets: map[string]int{},
+func NewKafka(kv *maelstrom.KV) Kafka {
+	return Kafka{kv: kv}
+}
+
+func (k *Kafka) readMessages(ctx context.Context, key string) ([]int, error) {
+	value, err := k.kv.Read(ctx, key)
+	if doesNotExist(err) {
+		return []int{}, nil
 	}
+	if err != nil {
+		return nil, err
+	}
+	jsonString, ok := value.(string)
+	if !ok {
+		return nil, fmt.Errorf("expected JSON encoded value")
+	}
+	var messages []int
+	if err := json.Unmarshal([]byte(jsonString), &messages); err != nil {
+		return nil, err
+	}
+	return messages, nil
+}
+
+func (k *Kafka) readOffsets(ctx context.Context) (map[string]int, error) {
+	value, err := k.kv.Read(ctx, "commit-offsets")
+	if doesNotExist(err) {
+		return map[string]int{}, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	jsonString, ok := value.(string)
+	if !ok {
+		return nil, fmt.Errorf("expected JSON encoded value")
+	}
+	var offsets map[string]int
+	if err := json.Unmarshal([]byte(jsonString), &offsets); err != nil {
+		return nil, err
+	}
+	return offsets, nil
 }
 
 func (k *Kafka) Append(key string, value int) int {
-	k.mu.Lock()
-	defer k.mu.Unlock()
-	k.messages[key] = append(k.messages[key], value)
-	return len(k.messages[key]) - 1
+	var id int
+	Retry(context.Background(), func(ctx context.Context) error {
+		messages, err := k.readMessages(ctx, key)
+		if err != nil {
+			return err
+		}
+		id = len(messages)
+		newMessages := append(messages, value)
+		return k.CompareAndSwap(ctx, key, messages, newMessages)
+	})
+	return id
 }
 
 func (k *Kafka) CommitOffsets(newOffsets map[string]int) {
-	k.mu.Lock()
-	defer k.mu.Unlock()
-	for key, val := range newOffsets {
-		k.committedOffsets[key] = val
-	}
+	Retry(context.Background(), func(ctx context.Context) error {
+		offsets, err := k.readOffsets(ctx)
+		if err != nil {
+			return err
+		}
+		for k, v := range offsets {
+			// If the key is already present in newOffsets, take it
+			// as the latest value.
+			if _, ok := newOffsets[k]; ok {
+				continue
+			}
+			// Otherwise, preserve the previous values.
+			newOffsets[k] = v
+		}
+		return k.CompareAndSwap(ctx, "commit-offsets", offsets, newOffsets)
+	})
 }
 
 func (k *Kafka) Offsets(keys ...string) map[string]int {
-	k.mu.Lock()
-	defer k.mu.Unlock()
-	offsets := make(map[string]int, len(keys))
+	offsets, _ := k.readOffsets(context.Background())
 	for _, key := range keys {
-		if _, ok := k.committedOffsets[key]; !ok {
+		// Delete any keys that weren't requested.
+		if _, ok := offsets[key]; ok {
 			continue
 		}
-		offsets[key] = k.committedOffsets[key]
+		delete(offsets, key)
 	}
 	return offsets
 }
 
 func (k *Kafka) Read(keyOffsets map[string]int) map[string][][]int {
-	k.mu.Lock()
-	defer k.mu.Unlock()
 	logs := make(map[string][][]int, len(keyOffsets))
 	for key, offset := range keyOffsets {
-		if offset >= len(k.messages[key]) {
+		messages, _ := k.readMessages(context.Background(), key)
+		// No new messages.
+		if offset >= len(messages) {
 			continue
 		}
-		vals := k.messages[key][offset:]
+		// Read all messages after offset.
+		vals := messages[offset:]
+		// Convert to [offset, message] pairs.
 		keyVals := make([][]int, len(vals))
 		for i := 0; i < len(keyVals); i++ {
 			keyVals[i] = []int{offset + i, vals[i]}
@@ -115,4 +171,26 @@ func (k *Kafka) Read(keyOffsets map[string]int) map[string][][]int {
 		logs[key] = keyVals
 	}
 	return logs
+}
+
+func (k *Kafka) CompareAndSwap(ctx context.Context, key string, from, to any) error {
+	return k.kv.CompareAndSwap(ctx, key, asJSON(from), asJSON(to), true)
+}
+
+func asJSON(v any) string {
+	jsonBytes, err := json.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+	return string(jsonBytes)
+}
+
+func doesNotExist(err error) bool {
+	if err == nil {
+		return false
+	}
+	if err, ok := err.(*maelstrom.RPCError); ok {
+		return err.Code == maelstrom.KeyDoesNotExist
+	}
+	return false
 }
